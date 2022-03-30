fd700e497268d6eeaff1300bae17a6acf0802002
spicious225 <ryanthelion.c@gmail.com>
2022-03-29 20:33:41 -0700

    change made

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyD4lqdOgFFMogltwKtp0_NsAqe9ibORFcQ",
+    authDomain: "fir-comp1800-d3fbd.firebaseapp.com",
+    projectId: "fir-comp1800-d3fbd",
+    storageBucket: "fir-comp1800-d3fbd.appspot.com",
+    messagingSenderId: "538364325885",
+    appId: "1:538364325885:web:0d79383caf7ac0d9c0f394",
+    measurementId: "G-5DKF6G8WX8"
 };
 
 //--------------------------------------------
